98855fcef171e794d8ff3454aeefc802e72a3172
Junaaa10 <arjuna_dewanta_33rpl@student.smktelkom-mlg.sch.id>
2024-11-18 11:34:01 +0700

    Add files via upload

---
+++ Parttime.java
@@ -0,0 +1,35 @@
+
+public class Parttime extends Teacher {
+    private int hoursWorked;
+    private int annualSalary;
+
+
+    public Parttime(String name, int age, int number, String subject, int annualSalary, int hoursWorked) {
+        super(name, age, number, subject);
+        this.annualSalary = annualSalary;
+        this.hoursWorked = hoursWorked;
+    }
+
+        public int getHoursWorked() {
+            return this.hoursWorked;
+        }
+    
+        public void setHoursWorked(int hoursWorked) {
+            this.hoursWorked = hoursWorked;
+        }
+    
+        public int getAnnualSalary() {
+            return this.annualSalary;
+        }
+    
+        public void setAnnualSalary(int annualSalary) {
+            this.annualSalary = annualSalary;
+        }
+
+        @Override
+        public void print() {
+        super.print(); 
+        System.out.println("Annual Salary: " + annualSalary); 
+        System.out.println("Hours Worked: " + hoursWorked); 
+    }
+}
